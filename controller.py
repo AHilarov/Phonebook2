467af3591bbd7acb9be41706d796663e17ba47c6
AHilarov <ahilarov@yandex.ru>
2023-03-14 21:36:22 +0300

    База данных, main и шаблончик контроллера

---
+++ controller.py
@@ -0,0 +1,25 @@
+
+
+
+def start():
+    while True:
+        choice = view.menu()
+        match choice:
+            case 1:
+                processor.open_file()
+            case 2:
+                processor.save_file()
+            case 3:                
+                view.show_contacts(processor.get_contacts())
+            case 4:
+                processor.new_contact(processor.get_contacts())
+            case 5:
+                processor.change_contact()
+            case 6:
+                processor.find_contact()
+                
+            case 7:
+                processor.delete_contact()
+            case 8:
+                pass
+                break
