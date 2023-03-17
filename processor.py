2f600edd29bd86e6895f32b9ba3c7a4183c57545
IrinaPenkina <nikolay_irina@mail.ru>
2023-03-18 00:43:16 +0300

    Commands modified

--- processor.py
+++ processor.py
@@ -1,70 +1,72 @@
-def open_file():
-    dictionary = {}
-    with open ('phonebook.txt', 'r') as data:
-        for line in data:
-            for i in range (len(line)):
-                if data[i] == ';':
-                    dictionary = {'name': data[:i]}
 
-def save_file():
-    file = input('Имя файла для сохранения (enter - имя файла для сохранения)')
-    if not file:
-        file = 'phonebook'
-    with open('phonebook.txt', 'w') as data:
-        data.writelines(' '.join(map(str, )))
-        data.write('\n')
+def find_contact():
+    with open (r'phonebook.txt', 'r', encoding="utf8") as file:
+        data = file.readlines()
+        contact = input("Задайте имя или фамилию контакта для поиска: ")
+        line_target = -1
+        for line in data:
+            if contact in line:
+                print(contact, "существует в справочнике." )
+                line_target = line
+                print("Полная запись контакта: ", line)
+        if line_target == -1:
+            print("Такого контакта нет.")
+
+
+# Первая версия - без задания изменяемой строки
+def change_contact():
+    with open(r'phonebook.txt', 'r', encoding="utf8") as file:
+        data = file.read()
+        print("Вот все записи справочника: \n")
+        print(data, "\n")
+        search_text = input("Введите запись, которую Вы хотете заменить: ")
+        replace_text = input("На что вы хотите заменить: ")
+        data = data.replace(search_text, replace_text)
+    with open(r'phonebook.txt', 'w', encoding="utf8") as file:
+        file.write(data)
+    print("Изменения внесены.")
     
 
+# Вторая версия - с заданием изменяемой строки
+# def change_contact():
+#     with open(r'phonebook.txt', 'r', encoding="utf8") as file:
+#         data = file.readlines()
+#         print("Вот все записи справочника: \n")
+#         for i, line in enumerate(data, 1):
+#             print(f'{i} : {line}')
+#         print(type(data))
+#         data = ''.join(str(x) for x in data)
+#         target_line = input("Введите номер строки, в которую Вы хотете внести изменения: ")
+#         search_text = input("Введите запись, которую Вы хотете заменить: ")
+#         replace_text = input("На что вы хотите заменить: ")
+#         for line in data:
+#             if line == target_line:
+#                 data = data.replace(search_text, replace_text)
+#     with open(r'phonebook.txt', 'w', encoding="utf8") as file:
+#         file.write(data)
+#     print("Изменения внесены.")
 
-def get_contacts():
-    pass
 
-def new_contact(dictionary, name, surname, number, comment):
-    pass
-
-
-def find_file():
-    with open ('phonebook.txt', 'r') as data:
-        contact = input('Пожалуйста, задайте фамилию адресата для поиска: ')
-        lines = data.readlines()
-
-        found = False
-        for line in lines:
+def delete_contact():
+    with open(r'phonebook.txt', 'r', encoding="utf8") as file:
+        data = file.readlines()
+        print(type(data))
+        print(data)
+        contact = input("Задайте имя или фамилию контакта для удаления: ")
+        line_del = -1
+        for line in data:
             if contact in line:
-                print("Вот контакт, который Вы искали: ", end=' ')
-                print(line)
-                found = True
-                break
-        if found == False:
-            print("Контакта с такой фамилией нет")
-    pass
+                print(contact, "существует в справочнике. Запись будет удалена." )
+                line_del = line
+        if line_del == -1:
+            print("Такого контакта нет. Справочник не будет изменен.")
+    with open(r'phonebook.txt', 'w', encoding="utf8") as file:
+        for line in data:
+            if line != line_del:
+                file.write(line)
+        data = ''.join(str(x) for x in data)
+    
 
-def change_file():
-    fin = open('phonebook.txt', 'r')
-    data = fin.read()
-    print("Вот все записи справочника: ", end=' ')
-    print(data)
-    text_1 = str(input("Введите запись, которую Вы хотете заменить: ", end=' '))
-    text_2 = str(input("На что вы хотите заменить: ", end=' '))
-    data = data.replace(text_1, text_2)
-    fin.close()
-    fin = open('phonebook.txt', 'w')
-    fin.write(data)
-    fin.close
-    pass
-
-def delete_file():
-    f = open('phonebook.txt', 'r')
-    lines = f.readlines()
-    f.close()
-    f = open('phonebook.txt', 'w')
-    line_del = int(input("Введите номер строки для удаления: "))
-    for line in lines:
-        if line != line_del:
-            f.write(line)
-    f.close()
-    pass
-
-def finish_file():
-    quit()
+def close_file():
+    exit()
 
